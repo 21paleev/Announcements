ba5ce56c4c491db42a1605432c5d2203446ea3d0
21paleev <paleev2121@gmail.com>
2021-09-28 14:53:28 +0300

    View_Binding

--- app/src/main/java/com/paleevigor/announcements/MainActivity.kt
+++ app/src/main/java/com/paleevigor/announcements/MainActivity.kt
@@ -2,59 +2,72 @@ package com.paleevigor.announcements
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import com.google.android.material.navigation.NavigationView
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.main_content.*
+import com.paleevigor.announcements.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var rootElement: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        rootElement = ActivityMainBinding.inflate(layoutInflater)
+        val view = rootElement.root
+        setContentView(view)
         init()
     }
 
     // функция инициализации кнопки в тулбаре
     // и установка слушателя нажатия злементов меню
-    private fun init(){
-        val toggle=ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.open, R.string.close)
-        drawerLayout.addDrawerListener(toggle)
+    private fun init() {
+        val toggle =
+            ActionBarDrawerToggle(
+                this,
+                rootElement.drawerLayout,
+                rootElement.mainContent.toolbar,
+                R.string.open,
+                R.string.close
+            )
+        rootElement.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
-        navView.setNavigationItemSelectedListener (this)
+        rootElement.navView.setNavigationItemSelectedListener(this)
 
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.id_my_ads->{
-Toast.makeText(this,"Presed id_my_ads",Toast.LENGTH_LONG).show()
+        when (item.itemId) {
+            R.id.id_my_ads -> {
+                Toast.makeText(this, "Presed id_my_ads", Toast.LENGTH_LONG).show()
             }
-            R.id.id_car->{
-                Toast.makeText(this,"Presed id_id_car",Toast.LENGTH_LONG).show()
+            R.id.id_car -> {
+                Toast.makeText(this, "Presed id_id_car", Toast.LENGTH_LONG).show()
             }
-            R.id.id_pc->{
-                Toast.makeText(this,"Presed id_pc",Toast.LENGTH_LONG).show()
+            R.id.id_pc -> {
+                Toast.makeText(this, "Presed id_pc", Toast.LENGTH_LONG).show()
             }
-            R.id.id_smartphone->{
-                Toast.makeText(this,"Presed id_smartphone",Toast.LENGTH_LONG).show()
+            R.id.id_smartphone -> {
+                Toast.makeText(this, "Presed id_smartphone", Toast.LENGTH_LONG).show()
             }
-            R.id.id_dm->{
-                Toast.makeText(this,"Presed id_dm",Toast.LENGTH_LONG).show()
+            R.id.id_dm -> {
+                Toast.makeText(this, "Presed id_dm", Toast.LENGTH_LONG).show()
             }
-            R.id.id_sign_up->{
-                Toast.makeText(this,"Presed id_sign_up",Toast.LENGTH_LONG).show()
+            R.id.id_sign_up -> {
+                Toast.makeText(this, "Presed id_sign_up", Toast.LENGTH_LONG).show()
             }
-            R.id.id_sign_in->{
-                Toast.makeText(this,"Presed id_sign_in",Toast.LENGTH_LONG).show()
+            R.id.id_sign_in -> {
+                Toast.makeText(this, "Presed id_sign_in", Toast.LENGTH_LONG).show()
             }
-            R.id.id_sign_out->{
-                Toast.makeText(this,"Presed id_sign_out",Toast.LENGTH_LONG).show()
+            R.id.id_sign_out -> {
+                Toast.makeText(this, "Presed id_sign_out", Toast.LENGTH_LONG).show()
             }
         }
-        drawerLayout.closeDrawer(GravityCompat.START)
+        rootElement.drawerLayout.closeDrawer(GravityCompat.START)
         return true
     }
 }
